01366c6b83e08c5c56cee34b3487ff7d87212d4b
free-flow-code <wildmuskaria8@gmail.com>
2024-01-23 16:40:57 +0300

    Add space garbage.

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ from random import randint, choice
 from curses_tools import draw_frame, read_controls, get_frame_size
 from itertools import cycle
 
+COROUTINES = []
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -18,6 +20,34 @@ def parse_arguments():
         default=50
     )
     return parser.parse_args()
+
+
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 2)
+    column = min(column, columns_number - 2)
+
+    row = 1
+
+    while row < rows_number:
+        draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
+
+
+async def fill_orbit_with_garbage(canvas, width_window, garbage_frames):
+    while True:
+        global COROUTINES
+        garbage_frame = choice(garbage_frames)
+        frame_rows, frame_columns = get_frame_size(garbage_frame)
+        column = randint(1, width_window - frame_columns)
+        garbage_coroutine = fly_garbage(canvas, column, garbage_frame)
+        COROUTINES.append(garbage_coroutine)
+        for _ in range(15):
+            await asyncio.sleep(0)
 
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
@@ -89,18 +119,29 @@ async def blink(canvas, row, column, offset_tics, symbol='*'):
 
 async def draw(canvas, number_stars):
     height_window, width_window = curses.window.getmaxyx(canvas)
+    garbage_files = [
+        'duck.txt',
+        'hubble.txt',
+        'lamp.txt',
+        'trash_large.txt',
+        'trash_small.txt',
+        'trash_xl.txt'
+    ]
     tic_timeout = 0.1
     stars = '+*.:'
     height_indent = 2
     width_indent = 2
     offset_tics = randint(1, 20)
+    global COROUTINES
+
     with (open('animations/rocket_frame_1.txt', 'r') as file1,
           open('animations/rocket_frame_2.txt', 'r') as file2):
         rocket_frame1 = file1.read()
         rocket_frame2 = file2.read()
         rocket_frames = [rocket_frame1, rocket_frame2]
+
     fire_coroutine = fire(canvas, height_window / 2, width_window / 2)
-    coroutines = [
+    COROUTINES = [
         blink(
             canvas,
             randint(1, height_window - height_indent),
@@ -109,14 +150,21 @@ async def draw(canvas, number_stars):
             choice(stars)
         ) for _ in range(0, number_stars)
     ]
-    coroutines.extend([fire_coroutine, animate_spaceship(canvas, height_window / 2, width_window / 2, rocket_frames)])
+    COROUTINES.extend([fire_coroutine, animate_spaceship(canvas, height_window / 2, width_window / 2, rocket_frames)])
+
+    garbage_frames = []
+    for file in garbage_files:
+        with open(f'animations/{file}', 'r') as garbage_file:
+            frame = garbage_file.read()
+        garbage_frames.append(frame)
+    COROUTINES.append(fill_orbit_with_garbage(canvas, width_window, garbage_frames))
 
     while True:
-        for coroutine in coroutines.copy():
+        for coroutine in COROUTINES.copy():
             try:
                 coroutine.send(None)
             except StopIteration:
-                coroutines.remove(coroutine)
+                COROUTINES.remove(coroutine)
         canvas.refresh()
         await asyncio.sleep(tic_timeout)
 
